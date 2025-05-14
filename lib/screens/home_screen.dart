65182ce98fa008c86a2509f2b8f44d853d808ca0
MoniqChege <quichege33@gmail.com>
2025-05-14 16:05:27 +0300

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../providers/movie_provider.dart';
 import '../providers/theme_provider.dart';
 import '../widgets/movie_category_section.dart';
+import 'movie_detail_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,19 +28,24 @@ class _HomeScreenState extends State<HomeScreen> {
     final movieProvider = Provider.of<MovieProvider>(context);
     final themeProvider = Provider.of<ThemeProvider>(context);
 
+    final isSearching = _searchController.text.isNotEmpty;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('🎬 Movie App'),
+        title: const Text('🎬 Movie Search'),
         centerTitle: true,
         actions: [
           Switch(
             value: themeProvider.isDarkMode,
-            onChanged: (val) => themeProvider.toggleTheme(val),
+            onChanged: (val) {
+              themeProvider.toggleTheme(val);
+            },
           ),
         ],
       ),
       body: Column(
         children: [
+          // Search input field
           Padding(
             padding: const EdgeInsets.all(12.0),
             child: TextField(
@@ -54,29 +60,73 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
               ),
-              onSubmitted: (query) => movieProvider.searchMovies(query),
+              onSubmitted: (query) {
+                movieProvider.searchMovies(query);
+              },
             ),
           ),
 
+          // Movie list or loading animation
           Expanded(
             child: movieProvider.isLoading
-                ? const Center(child: CircularProgressIndicator())
+                ? const Center(
+              child: CircularProgressIndicator(),
+            )
+                : isSearching
+                ? ListView.builder(
+              itemCount: movieProvider.movies.length,
+              itemBuilder: (context, index) {
+                final movie = movieProvider.movies[index];
+                return ListTile(
+                  leading: movie.posterPath.isNotEmpty
+                      ? Image.network(
+                    'https://image.tmdb.org/t/p/w92${movie.posterPath}',
+                    width: 50,
+                    fit: BoxFit.cover,
+                  )
+                      : const Icon(Icons.image_not_supported),
+                  title: Text(movie.title),
+                  subtitle: Text(
+                    movie.overview,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(
+                      movie.isFavorite ? Icons.favorite : Icons.favorite_border,
+                      color: movie.isFavorite ? Colors.red : null,
+                    ),
+                    onPressed: () {
+                      movieProvider.toggleFavorite(movie);
+                    },
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => MovieDetailScreen(movie: movie),
+                      ),
+                    );
+                  },
+                );
+              },
+            )
                 : ListView(
               children: [
                 MovieCategorySection(
-                  title: '📽️ Latest Releases',
+                  title: 'Latest',
                   movies: movieProvider.latestMovies,
                 ),
                 MovieCategorySection(
-                  title: '🔥 Most Popular',
+                  title: 'Popular',
                   movies: movieProvider.popularMovies,
                 ),
                 MovieCategorySection(
-                  title: '🏆 Top Rated',
+                  title: 'Top Rated',
                   movies: movieProvider.topRatedMovies,
                 ),
                 MovieCategorySection(
-                  title: '⏳ Upcoming',
+                  title: 'Upcoming',
                   movies: movieProvider.upcomingMovies,
                 ),
               ],
